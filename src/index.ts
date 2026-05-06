f2e6bcb7754419b2be61d26ad8407fae8cdaa977
Nur Mohammod <nurm98371@gmail.com>
2026-05-06 13:24:11 +0600

    Add

---
+++ src/index.ts
@@ -0,0 +1,47 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+const typeDefs = `#graphql
+  type Book {
+    title: String
+    author: String
+  }
+
+  type Query {
+    books: [Book]
+    _health: String!
+  }
+`;
+
+const books = [
+  {
+    title: "The Awakening",
+    author: "Kate Chopin",
+  },
+  {
+    title: "City of Glass",
+    author: "Paul Auster",
+  },
+];
+
+const resolvers = {
+  Query: {
+    books: () => books,
+    _health: () => "ok",
+  },
+};
+
+const main=async () => {
+  const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
+
+console.log(`🚀  Server ready at: ${url}`);
+}
+main()
