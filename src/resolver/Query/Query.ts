b704d1ea0f1dc24026fcd02558c6066155727d10
Nur Mohammod <nurm98371@gmail.com>
2026-05-08 15:33:43 +0600

    Add

--- src/resolver/Query/Query.ts
+++ src/resolver/Query/Query.ts
@@ -1,21 +1,18 @@
 export const Query = {
-me:async (parent: any, args: any, { prisma, userInfo }: any) => {
-
-},
-users: async (parent: any, args: any, { prisma }: any) => {
+  me: async (parent: any, args: any, { prisma, userInfo }: any) => {
+    return await prisma.user.findUnique({
+      where: { id: userInfo.userId },
+    });
+  },
+  users: async (parent: any, args: any, { prisma }: any) => {
     return await prisma.user.findMany();
   },
   posts: async (parent: any, args: any, { prisma }: any) => {
-    return await prisma.post.findMany(
-      {
-        where:{
-          published:true
-        },
-        oderBy:[
-          {createdAt:"desc"}
-        
-        ]
-      }
-    );
+    return await prisma.post.findMany({
+      where: {
+        published: true,
+      },
+      oderBy: [{ createdAt: "desc" }],
+    });
   },
 };
