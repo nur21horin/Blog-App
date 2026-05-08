d075b83aff61675b577fdc3a75eb5627eba47e9b
Nur Mohammod <nurm98371@gmail.com>
2026-05-08 15:32:57 +0600

    Add

--- src/resolver/Query/Query.ts
+++ src/resolver/Query/Query.ts
@@ -1,5 +1,8 @@
 export const Query = {
-  me: async (parent: any, args: any, { prisma }: any) => {
+me:async (parent: any, args: any, { prisma, userInfo }: any) => {
+
+},
+users: async (parent: any, args: any, { prisma }: any) => {
     return await prisma.user.findMany();
   },
   posts: async (parent: any, args: any, { prisma }: any) => {
